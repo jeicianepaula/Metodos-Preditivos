042442bf23503ac5af6f2331613d0be4baec1faf
Jeiciane de Souza Paula <58893940+jeicianepaula@users.noreply.github.com>
2021-11-25 21:21:39 -0300

    Update and rename ETS 9.0.R to ETS.R

--- ETS.R
+++ ETS.R
@@ -4,7 +4,7 @@ library(xlsx)
 library(fpp2)
 
 #--------------------------------------------------EXPORTACAODE DADOS-------------------------------------------------------------------------------------------
-#Expota e separa os dados tipo date dos dados para formaÁ„o das series temporais
+#Expota e separa os dados tipo date dos dados para forma√ß√£o das series temporais
 Dados= read.xlsx(file.choose(), header = F,sheetIndex = 1)
 Data= Dados[,1]
 Dados1= Dados[,-1]
@@ -21,27 +21,27 @@ autoplot(serie)+ ylim(c(17.5, 30)) +labs( title = "Serie Temporal") + theme_mini
         theme(plot.title = element_text(hjust = 0.5)) + geom_line(size = 1)
 
 #---------------------------------------------------------ETS----------------------------------------------------------------------------------------------
-ETS = function (x,Data_IN,Data_OUT,Dados_reais_OUT){
+ETS = function (x,Data_IN,Data_OUT,Dados_reais_OUT, h){
  
-  #Faz a modelagem e previs„o
-  ETS = ets(serie) %>% forecast(h = 4)
+  #Faz a modelagem e previs√£o
+  ETS = ets(x) %>% forecast(h = h)
   print(ETS[["model"]])
   
   #cria dois arquivos do tipo data.frame para armazenar as informacoes de previsao dentro e fora da amostra
-  Previs„o_D = data.frame(Data_IN  ,Dados_reais_IN = (ETS[["x"]]),  Previs„o_IN = (ETS[["fitted"]]))
-  Previs„o_F= data.frame(Data_OUT, Dados_reais_OUT, Previs„o_OUT = (ETS[["mean"]]))
+  Previs√£o_D = data.frame(Data_IN  ,Dados_reais_IN = (ETS[["x"]]),  Previs√£o_IN = (ETS[["fitted"]]))
+  Previs√£o_F= data.frame(Data_OUT, Dados_reais_OUT, Previs√£o_OUT = (ETS[["mean"]]))
  
   #calcula os erros de previsao dentro e fora da amostra pelas metricas de erros e amarzena em um data.frame
-  Erro_IN = data.frame(MAPE_IN = mape(Previs„o_D$Dados_reais_IN, Previs„o_D$Previs„o_IN)*100,
-                       MAE_IN = mae(Previs„o_D$Dados_reais_IN, Previs„o_D$Previs„o_IN),
-                       RMSE_IN =rmse(Previs„o_D$Dados_reais_IN, Previs„o_D$Previs„o_IN))
+  Erro_IN = data.frame(MAPE_IN = mape(Previs√£o_D$Dados_reais_IN, Previs√£o_D$Previs√£o_IN)*100,
+                       MAE_IN = mae(Previs√£o_D$Dados_reais_IN, Previs√£o_D$Previs√£o_IN),
+                       RMSE_IN =rmse(Previs√£o_D$Dados_reais_IN, Previs√£o_D$Previs√£o_IN))
                        print(Erro_IN)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-  Erro_OUT = data.frame(MAPE_OUT = mape(Previs„o_F$Dados_reais_OUT, Previs„o_F$Previs„o_OUT)*100,
-                        MAE_OUT = mae(Previs„o_F$Dados_reais_OUT, Previs„o_F$Previs„o_OUT),
-                        RMSE_OUT = rmse(Previs„o_F$Dados_reais_OUT, Previs„o_F$Previs„o_OUT))
+  Erro_OUT = data.frame(MAPE_OUT = mape(Previs√£o_F$Dados_reais_OUT, Previs√£o_F$Previs√£o_OUT)*100,
+                        MAE_OUT = mae(Previs√£o_F$Dados_reais_OUT, Previs√£o_F$Previs√£o_OUT),
+                        RMSE_OUT = rmse(Previs√£o_F$Dados_reais_OUT, Previs√£o_F$Previs√£o_OUT))
                         print(Erro_OUT)
   
-  #Armazena os par‚metros da momdelagem em um data.frame
+  #Armazena os par√¢metros da momdelagem em um data.frame
   Parametros = c(ETS[["model"]][["method"]],ETS[["model"]][["loglik"]], ETS[["model"]][["aic"]], ETS[["model"]][["bic"]], ETS[["model"]][["aicc"]],
                  ETS[["model"]][["par"]][["alpha"]],
                  ETS[["model"]][["par"]][["l"]])
@@ -53,12 +53,12 @@ ETS = function (x,Data_IN,Data_OUT,Dados_reais_OUT){
   
   sheet = createSheet(wb, "Prev.in")
   cs1 <- CellStyle(wb) + Font(wb, isBold=TRUE) + Border()  # header
-  addDataFrame(Previs„o_D, sheet=sheet, startColumn=1, row.names= FALSE, colnamesStyle=cs1)
+  addDataFrame(Previs√£o_D, sheet=sheet, startColumn=1, row.names= FALSE, colnamesStyle=cs1)
   addDataFrame(Erro_IN, sheet=sheet, startColumn=4, row.names= FALSE, colnamesStyle=cs1)
   sheet = createSheet(wb, "Prev.out")
-  addDataFrame(Previs„o_F, sheet=sheet, startColumn=1, row.names= FALSE, colnamesStyle=cs1)
+  addDataFrame(Previs√£o_F, sheet=sheet, startColumn=1, row.names= FALSE, colnamesStyle=cs1)
   addDataFrame(Erro_OUT, sheet=sheet, startColumn=4, row.names= FALSE, colnamesStyle=cs1)
-  sheet = createSheet(wb, "Par‚metros")
+  sheet = createSheet(wb, "Par√¢metros")
   addDataFrame(Parametros, sheet=sheet, startColumn=1, col.names= FALSE, colnamesStyle=cs1)
   
   saveWorkbook(wb, "ETS.xlsx")
@@ -66,5 +66,5 @@ ETS = function (x,Data_IN,Data_OUT,Dados_reais_OUT){
 
 }
   
-ETS(serie,Data[1:16],Data[17:20],Dados[17:20,2])
+ETS(serie,Data[1:16],Data[17:20],Dados[17:20,2], 24)
 
