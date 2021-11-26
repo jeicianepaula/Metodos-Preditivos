e91e7e13a66a28f4164babac9753e10bc2fe1ea3
Jeiciane de Souza Paula <58893940+jeicianepaula@users.noreply.github.com>
2021-11-25 21:22:21 -0300

    Update and rename GARCH 9.0.R to GARCH.R

--- GARCH.R
+++ GARCH.R
@@ -6,7 +6,7 @@ library(rugarch)
 library(erer)
 
 #--------------------------------------------------EXPORTACAODE DADOS-------------------------------------------------------------------------------------------
-  #Expota e separa os dados tipo date dos dados para formaÁ„o das series temporais
+  #Expota e separa os dados tipo date dos dados para forma√ß√£o das series temporais
   Ex = read.xlsx(file.choose(), header = T,sheetIndex = 1)
   Data= Ex[,1]
   Dados= Ex[,-1]
@@ -16,35 +16,35 @@ library(erer)
   serie = ts(Dados[,1], start = c(2000,1), end = c(2020,2),frequency = 12) %>% window(end = c(2012,12))
 
 #---------------------------------------------------------GARCH----------------------------------------------------------------------------------------------
-GARCH = function (x,Data_IN,Data_OUT,Dados_reais_OUT){
+GARCH = function (x,Data_IN,Data_OUT,Dados_reais_OUT,h){
     
-    #Faz a modelagem e previs„o 
+    #Faz a modelagem e previs√£o 
     garchSpec = ugarchspec( variance.model=list(model="sGARCH",garchOrder=c(1,1)),
                             mean.model=list(armaOrder=c(1,1)), distribution.model="std")
     GARCH = ugarchfit(spec=garchSpec, data=x)
-    prevgarch= ugarchforecast(GARCH, n.ahead = 24, n.roll = 0 )
+    prevgarch= ugarchforecast(GARCH, n.ahead = h, n.roll = 0 )
     
     #cria dois arquivos do tipo data.frame para armazenar as informacoes de previsao dentro e fora da amostra
     
-    Previs„o_D = data.frame(Data_IN, Dados_reais_IN = (prevgarch@model[["modeldata"]][["data"]]), Previs„o_IN = (GARCH@fit[["fitted.values"]]))
-    Previs„o_F= data.frame(Data_OUT, Dados_reais_OUT , Previs„o_OUT = prevgarch@forecast[["seriesFor"]])
-    names(Previs„o_F)[3]=c("Previs„o_OUT")
+    Previs√£o_D = data.frame(Data_IN, Dados_reais_IN = (prevgarch@model[["modeldata"]][["data"]]), Previs√£o_IN = (GARCH@fit[["fitted.values"]]))
+    Previs√£o_F= data.frame(Data_OUT, Dados_reais_OUT , Previs√£o_OUT = prevgarch@forecast[["seriesFor"]])
+    names(Previs√£o_F)[3]=c("Previs√£o_OUT")
     
     #calcula os erros de previsao dentro e fora da amostra pelas metricas de erros e amarzena em um data.frame
-    MAPE_IN = mape(Previs„o_D$Dados_reais_IN, Previs„o_D$Previs„o_IN)*100
-    MAE_IN = mae(Previs„o_D$Dados_reais_IN, Previs„o_D$Previs„o_IN)
-    RMSE_IN =rmse(Previs„o_D$Dados_reais_IN, Previs„o_D$Previs„o_IN)
+    MAPE_IN = mape(Previs√£o_D$Dados_reais_IN, Previs√£o_D$Previs√£o_IN)*100
+    MAE_IN = mae(Previs√£o_D$Dados_reais_IN, Previs√£o_D$Previs√£o_IN)
+    RMSE_IN =rmse(Previs√£o_D$Dados_reais_IN, Previs√£o_D$Previs√£o_IN)
     Erro_IN = data.frame(MAPE_IN, MAE_IN, RMSE_IN)
     print(Erro_IN)
     
-    Previs„o = prevgarch@forecast[["seriesFor"]]
-    MAPE_OUT = mape(Previs„o_F$Dados_reais_OUT,Previs„o)*100
-    MAE_OUT = mae(Previs„o_F$Dados_reais_OUT,Previs„o)
-    RMSE_OUT = rmse(Previs„o_F$Dados_reais_OUT,Previs„o) 
+    Previs√£o = prevgarch@forecast[["seriesFor"]]
+    MAPE_OUT = mape(Previs√£o_F$Dados_reais_OUT,Previs√£o)*100
+    MAE_OUT = mae(Previs√£o_F$Dados_reais_OUT,Previs√£o)
+    RMSE_OUT = rmse(Previs√£o_F$Dados_reais_OUT,Previs√£o) 
     Erro_OUT = data.frame(MAPE_OUT,MAE_OUT, RMSE_OUT)
     print(Erro_OUT)
     
-    #Armazena os par‚metros da momdelagem em um data.frame
+    #Armazena os par√¢metros da momdelagem em um data.frame
     Parametros = data.frame(GARCH@fit[["coef"]])
     
     #o planilhador cria e exporta os dados obtidos para um planilha de excel
@@ -52,14 +52,14 @@ GARCH = function (x,Data_IN,Data_OUT,Dados_reais_OUT){
     
     sheet = createSheet(wb, "Prev.in")
     cs1 <- CellStyle(wb) + Font(wb, isBold=TRUE) + Border()  # header
-    addDataFrame(Previs„o_D, sheet=sheet, startColumn=1, row.names= FALSE, colnamesStyle=cs1)
+    addDataFrame(Previs√£o_D, sheet=sheet, startColumn=1, row.names= FALSE, colnamesStyle=cs1)
     addDataFrame(Erro_IN, sheet=sheet, startColumn=4, row.names= FALSE, colnamesStyle=cs1)
     
     sheet = createSheet(wb, "Prev.out")
-    addDataFrame(Previs„o_F, sheet=sheet, startColumn=1, row.names= FALSE, colnamesStyle=cs1)
+    addDataFrame(Previs√£o_F, sheet=sheet, startColumn=1, row.names= FALSE, colnamesStyle=cs1)
     addDataFrame(Erro_OUT, sheet=sheet, startColumn=4, row.names= FALSE, colnamesStyle=cs1)
     
-    sheet = createSheet(wb, "Par‚metros")
+    sheet = createSheet(wb, "Par√¢metros")
     addDataFrame(Parametros, sheet=sheet, startColumn=1, col.names= FALSE, colnamesStyle=cs1)
     
     saveWorkbook(wb, "GARCH.xlsx")
@@ -67,6 +67,6 @@ GARCH = function (x,Data_IN,Data_OUT,Dados_reais_OUT){
     return(prevgarch@forecast[["seriesFor"]])
 }
 
-GARCH(serie,Data[1:156],Data[157:180],Dados[157:180,1])
+GARCH(serie,Data[1:156],Data[157:180],Dados[157:180,1],24)
   
   
