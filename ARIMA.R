ce9849d85199f55ca45eb9a2f55052fbcede4e55
Jeiciane de Souza Paula <58893940+jeicianepaula@users.noreply.github.com>
2021-11-25 21:11:42 -0300

    Update and rename ARIMA 9.0.R to ARIMA.R

--- ARIMA.R
+++ ARIMA.R
@@ -1,12 +1,12 @@
 #ATIVANDO AS BIBLIOTECAS *Caso necessite instalar utilize o comando install.packages()*
 library(Metrics) #METRICAS DE ERRO
-library(xlsx) #IMPOTA«√O DO DADOS 
-library(fpp2) # MODELOS DE PREVIS√O LINEARES
-library(rugarch) #MODELOS DE PREVIS’ N√O LINEARES
+library(xlsx) #IMPOTA√á√ÉO DO DADOS 
+library(fpp2) # MODELOS DE PREVIS√ÉO LINEARES
+library(rugarch) #MODELOS DE PREVIS√ï N√ÉO LINEARES
 library(erer) #econometria
 
 #--------------------------------------------------EXPORTACAO  DE DADOS-------------------------------------------------------------------------------------------
-#Expota e separa os dados tipo date dos dados para formaÁ„o das series temporais
+#Expota e separa os dados tipo date dos dados para forma√ß√£o das series temporais
 Ex = read.xlsx(file.choose(), header = T,sheetIndex = 1)
 Data= Ex[,1]
 Dados= Ex[,-1]
@@ -16,27 +16,27 @@ Dados= Ex[,-1]
 serie = ts(Dados[,1], start = c(2000,1), end = c(2020,2),frequency = 12) %>% window(end = c(2012,12))
 
 #---------------------------------------------------------ARIMA/SARIMA----------------------------------------------------------------------------------------------
-ARIMA = function (x,Data_IN,Data_OUT,Dados_reais_OUT){
+ARIMA = function (x,Data_IN,Data_OUT,Dados_reais_OUT, h){ #h = horizonte de previs√£o
   
-    #Faz a modelagem e previs„o  
-    ARIMA = serie %>% auto.arima(trace=T, stepwise = F, approximation = F) %>% forecast(h=24,level = c(85,95))
+    #Faz a modelagem e previs√£o  
+    ARIMA = x %>% auto.arima(trace=T, stepwise = F, approximation = F) %>% forecast(h=h,level = c(85,95))
     print(ARIMA[["model"]])
     
     #cria dois arquivos do tipo data.frame para armazenar as informacoes de previsao dentro e fora da amostra
-    Previs„o_D = data.frame(Data_IN, Dados_reais_IN = ARIMA[["model"]][["x"]], Previs„o_IN = ARIMA[["fitted"]])
-    Previs„o_F= data.frame(Data_OUT, Dados_reais_OUT, Previs„o_OUT = (ARIMA[["mean"]])) 
+    Previs√£o_D = data.frame(Data_IN, Dados_reais_IN = ARIMA[["model"]][["x"]], Previs√£o_IN = ARIMA[["fitted"]])
+    Previs√£o_F= data.frame(Data_OUT, Dados_reais_OUT, Previs√£o_OUT = (ARIMA[["mean"]])) 
     
     #calcula os erros de previsao dentro e fora da amostra pelas metricas de erros e amarzena em um data.frame
-    Erro_IN = data.frame(MAPE_IN = mape(Previs„o_D$Dados_reais_IN, Previs„o_D$Previs„o_IN)*100,
-                         MAE_IN = mae(Previs„o_D$Dados_reais_IN, Previs„o_D$Previs„o_IN),
-                         RMSE_IN =rmse(Previs„o_D$Dados_reais_IN, Previs„o_D$Previs„o_IN))
+    Erro_IN = data.frame(MAPE_IN = mape(Previs√£o_D$Dados_reais_IN, Previs√£o_D$Previs√£o_IN)*100,
+                         MAE_IN = mae(Previs√£o_D$Dados_reais_IN, Previs√£o_D$Previs√£o_IN),
+                         RMSE_IN =rmse(Previs√£o_D$Dados_reais_IN, Previs√£o_D$Previs√£o_IN))
                          print(Erro_IN)
-    Erro_OUT = data.frame(MAPE_OUT = mape(Previs„o_F$Dados_reais_OUT, Previs„o_F$Previs„o_OUT)*100,
-                          MAE_OUT = mae(Previs„o_F$Dados_reais_OUT, Previs„o_F$Previs„o_OUT),
-                          RMSE_OUT = rmse(Previs„o_F$Dados_reais_OUT, Previs„o_F$Previs„o_OUT))
+    Erro_OUT = data.frame(MAPE_OUT = mape(Previs√£o_F$Dados_reais_OUT, Previs√£o_F$Previs√£o_OUT)*100,
+                          MAE_OUT = mae(Previs√£o_F$Dados_reais_OUT, Previs√£o_F$Previs√£o_OUT),
+                          RMSE_OUT = rmse(Previs√£o_F$Dados_reais_OUT, Previs√£o_F$Previs√£o_OUT))
                           print(Erro_OUT)
                           
-    #Armazena os par‚metros da momdelagem em um data.frame
+    #Armazena os par√¢metros da momdelagem em um data.frame
     Parametros_modelos= c(ARIMA[["method"]],ARIMA[["model"]][["sigma2"]], 
                           ARIMA[["model"]][["aic"]],ARIMA[["model"]][["aicc"]], 
                           ARIMA[["model"]][["bic"]])
@@ -49,17 +49,17 @@ ARIMA = function (x,Data_IN,Data_OUT,Dados_reais_OUT){
     
     sheet = createSheet(wb, "Prev.in")
     cs1 <- CellStyle(wb) + Font(wb, isBold=TRUE) + Border()  # header
-    addDataFrame(Previs„o_D, sheet=sheet, startColumn=1, row.names= FALSE, colnamesStyle=cs1)
+    addDataFrame(Previs√£o_D, sheet=sheet, startColumn=1, row.names= FALSE, colnamesStyle=cs1)
     addDataFrame(Erro_IN, sheet=sheet, startColumn=4, row.names= FALSE, colnamesStyle=cs1)
     sheet = createSheet(wb, "Prev.out")
-    addDataFrame(Previs„o_F, sheet=sheet, startColumn=1, row.names= FALSE, colnamesStyle=cs1)
+    addDataFrame(Previs√£o_F, sheet=sheet, startColumn=1, row.names= FALSE, colnamesStyle=cs1)
     addDataFrame(Erro_OUT, sheet=sheet, startColumn=4, row.names= FALSE, colnamesStyle=cs1)
-    sheet = createSheet(wb, "Par‚metros")
+    sheet = createSheet(wb, "Par√¢metros")
     addDataFrame(Parametros, sheet=sheet, startColumn=1, col.names= FALSE, colnamesStyle=cs1)
     saveWorkbook(wb, "ARIMA.xlsx")
     
     return(ARIMA[["mean"]])
 }
 
-ARIMA(serie,Data[1:156],Data[157:180],Dados[157:180,1])
+ARIMA(serie,Data[1:156],Data[157:180],Dados[157:180,1], 24)
 
